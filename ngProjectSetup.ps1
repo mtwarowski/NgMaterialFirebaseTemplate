bfbf9e4a948acc360daccd51b1341bc064e93ab4
Michal.Twarowski <Michal.Twarowski@UK00342>
2017-07-26 18:12:59 +0100

    uncomment script

--- ngProjectSetup.ps1
+++ ngProjectSetup.ps1
@@ -4,31 +4,31 @@ param(
 ) 
 $ErrorActionPreference = "Stop"
 
-# if ((Get-Command 'npm' -errorAction SilentlyContinue) -eq $null)
-# {
-# 	Write-Error "Npm is not intalled. Please install npm and run the script again";
-# }
+if ((Get-Command 'npm' -errorAction SilentlyContinue) -eq $null)
+{
+	Write-Error "Npm is not intalled. Please install npm and run the script again";
+}
 
-# if ((Get-Command 'ng' -errorAction SilentlyContinue) -eq $null)
-# {
-# 	#install ng if not exists
-# 	npm install -g @angular/cli
-# }
+if ((Get-Command 'ng' -errorAction SilentlyContinue) -eq $null)
+{
+	#install ng if not exists
+	npm install -g @angular/cli
+}
 
-# # npm install -g typings
-# # npm install -g typescript
+# npm install -g typings
+# npm install -g typescript
 
 
-# #create default Project
-# ng new $ProjectName
+#create default Project
+ng new $ProjectName
 Set-Location $ProjectName
 
-# #add materials and animations 
-# #https://coursetro.com/posts/code/67/Angular-4-Material-Tutorial
-# npm install --save @angular/material @angular/animations
+#add materials and animations 
+#https://coursetro.com/posts/code/67/Angular-4-Material-Tutorial
+npm install --save @angular/material @angular/animations
 
-# #add firebase
-# #https://progblog.io/Angular-2-Firebase-Tutorial-Part-1-Create-a-Firebase-3-CRUD-app-with-Angular-CLI/
-# npm install angularfire2 firebase --save
+#add firebase
+#https://progblog.io/Angular-2-Firebase-Tutorial-Part-1-Create-a-Firebase-3-CRUD-app-with-Angular-CLI/
+npm install angularfire2 firebase --save
 
 Copy-Item ../app.module.ts ./src/app  -Force
